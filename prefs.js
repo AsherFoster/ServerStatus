2963c70a9d679b08a12685384644a94ad9e51fe3
Asher Foster <asher@developster.io>
2015-12-09 10:52:04 +1300

    Doing some stuff

--- prefs.js
+++ prefs.js
@@ -10,10 +10,13 @@ var prefs = {
     "timeout":5000, // How long to wait for testPage to return status code
     "testPage":"/test.php?url=", // Page to test if server is up. Page must return status code in plain text
     "checkRate":2000, // How often (in milliseconds) should the servers be checked
+    "temp":{
+        "url":"http://stationdata.wunderground.com/cgi-bin/stationlookup?format=json&maxage=10&station=ICANTERB8&units=metric&v=2.0",
+    },
     "threshold":2, // How many attempts must fail before updating status
     "slack":{
-        "enabled":true, // Whether to send messages to slack
-        "webhook":"https://hooks.slack.com/services/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", // Slack webhook. See "Webhook intergrations" in slack intergrations for more info
+        "enabled":false, // Whether to send messages to slack
+        "webhook":"http://example.com", // Slack webhook. See "Webhook intergrations" in slack intergrations for more info
         "threshold":3 // How many attempts must fail before alerting Slack
     },
     "servers": [ // List of servers to check
@@ -29,11 +32,11 @@ var prefs = {
             "name": "Developster Beta",
             "url": "http://beta.developster.io"
         },
-        {
-            "name": "Developster Alpha",
-            "url": "http://alpha.developster.io",
-            "alert":false
-        },
+//        {
+//            "name": "Developster Alpha",
+//            "url": "http://alpha.developster.io",
+//            "alert":false
+//        },
         {
             "name": "Innovate For ChCh",
             "url": "http://innovateforchch.nz"
@@ -55,7 +58,7 @@ var prefs = {
         "online":"#0F0", // Online server circle color
         "abnormal":"#F70", // Abnomal color
         "offline":"#F00", // Offline color
-        "background": "#0000FF", // Full page background
+        "background": "#000 no-repeat;background-size:cover", // Full page background
         "text":"#FFF" // Color of all text
     }
 }
